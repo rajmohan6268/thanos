2badca18021818c079a6df79c55b4d5e637607e1
Bartlomiej Plotka <bwplotka@gmail.com>
2021-02-24 23:48:57 +0100

    Replaces errutil with tools/pkg/merrors; Fixes very ugly misuse of multi-error.
    Previous multi-error implementation could cause very ugly bug of returnig empty multi-error
    that should be treated as success not error by API, but if .Err() is not invoked it will be
    used as non nil error.
    
    There were 9 places where we had this bug in handler due to MultiError lib allowing to do so.
    
    Signed-off-by: Bartlomiej Plotka <bwplotka@gmail.com>

--- pkg/compact/compact_test.go
+++ pkg/compact/compact_test.go
@@ -6,11 +6,11 @@ package compact
 import (
 	"testing"
 
+	"github.com/efficientgo/tools/core/pkg/merrors"
 	"github.com/pkg/errors"
 	"github.com/prometheus/prometheus/tsdb"
 
 	"github.com/thanos-io/thanos/pkg/block/metadata"
-	"github.com/thanos-io/thanos/pkg/errutil"
 	"github.com/thanos-io/thanos/pkg/testutil"
 )
 
@@ -32,12 +32,12 @@ func TestHaltMultiError(t *testing.T) {
 	haltErr := halt(errors.New("halt error"))
 	nonHaltErr := errors.New("not a halt error")
 
-	errs := errutil.MultiError{nonHaltErr}
-	testutil.Assert(t, !IsHaltError(errs), "should not be a halt error")
+	errs := merrors.New(nonHaltErr)
+	testutil.Assert(t, !IsHaltError(errs.Err()), "should not be a halt error")
 
 	errs.Add(haltErr)
-	testutil.Assert(t, IsHaltError(errs), "if any halt errors are present this should return true")
-	testutil.Assert(t, IsHaltError(errors.Wrap(errs, "wrap")), "halt error with wrap")
+	testutil.Assert(t, IsHaltError(errs.Err()), "if any halt errors are present this should return true")
+	testutil.Assert(t, IsHaltError(errors.Wrap(errs.Err(), "wrap")), "halt error with wrap")
 
 }
 
@@ -45,16 +45,15 @@ func TestRetryMultiError(t *testing.T) {
 	retryErr := retry(errors.New("retry error"))
 	nonRetryErr := errors.New("not a retry error")
 
-	errs := errutil.MultiError{nonRetryErr}
-	testutil.Assert(t, !IsRetryError(errs), "should not be a retry error")
+	errs := merrors.New(nonRetryErr)
+	testutil.Assert(t, !IsRetryError(errs.Err()), "should not be a retry error")
 
-	errs = errutil.MultiError{retryErr}
-	testutil.Assert(t, IsRetryError(errs), "if all errors are retriable this should return true")
+	errs = merrors.New(retryErr)
+	testutil.Assert(t, IsRetryError(errs.Err()), "if all errors are retriable this should return true")
+	testutil.Assert(t, IsRetryError(errors.Wrap(errs.Err(), "wrap")), "retry error with wrap")
 
-	testutil.Assert(t, IsRetryError(errors.Wrap(errs, "wrap")), "retry error with wrap")
-
-	errs = errutil.MultiError{nonRetryErr, retryErr}
-	testutil.Assert(t, !IsRetryError(errs), "mixed errors should return false")
+	errs = merrors.New(nonRetryErr, retryErr)
+	testutil.Assert(t, !IsRetryError(errs.Err()), "mixed errors should return false")
 }
 
 func TestRetryError(t *testing.T) {
